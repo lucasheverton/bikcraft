a761aecc32bf10617be3a4a015dad271a6f05da3
Lucas <lucas.santos@wecontent.com.br>
2021-03-20 23:20:55 -0300

    add plugin slide js

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+
+if (window.SimpleSlide) {
+    new SimpleSlide({
+        slide: "quote", // nome do atributo data-slide="principal"   
+        time: 4000 // tempo de transição dos slides
+    });
+
+    new SimpleSlide({
+        slide: 'portifoio',
+        nav: true,
+        time: 5000
+    });
+}
