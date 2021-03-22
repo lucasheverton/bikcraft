705223552410b3b9fd84a2a163321cbd54b5f529
Lucas <lucas.santos@wecontent.com.br>
2021-03-21 21:11:25 -0300

    fix error form

--- assets/js/index.js
+++ assets/js/index.js
@@ -21,6 +21,6 @@ if (window.SimpleForm) {
         form: ".form_php", // seletor do formulário
         button: "#enviar", // seletor do botão
         erro: "<div id='form-erro'><h2>Erro no envio :(</h2><p>Um erro ocorreu, tente enviar um email para <strong>lucas.7heverton@hotmail.com</strong>.</p></div>", // mensagem de erro
-        sucesso: "<div id='form-sucesso'><h2>Formulário enviado com sucesso</h2><p>Em breve eu entro em contato com você.</p></div>", // mensagem de sucesso
+        sucesso: "<div id='form-sucesso'><h2>Formulário enviado com sucesso</h2><p>Em breve eu entro em contato com você.</p></div>" // mensagem de sucesso
       });
 }
